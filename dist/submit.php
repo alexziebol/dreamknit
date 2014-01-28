9f329e0712ba0b8b9c16f0acdc31ef3fdaf8a05c
alex.ziebol <alex.ziebol@gmail.com>
2014-01-28 15:32:51 -0800

    PHP now able to insert information into database

--- dist/submit.php
+++ dist/submit.php
@@ -1,25 +1,33 @@
 <?php
 
 //Database Credentials
-$host = 'localhost';
-$database = 'dreamknit';
-$username = 'root';
-$password = 'root';
- 
-try {
-  $DBH = new PDO("mysql:host=$host;dbname=$database", $username, $password);
- 
-}
-catch(PDOException $e) {
-    echo $e->getMessage();
-}
+$host = 'localhost'; // host name
+$database = 'dreamknit'; // database name
+$username = 'root'; // username
+$password = 'root'; // password
 
+// Get values from form and assign to variables
 $object = $_POST['object'];
 $attitude = $_POST['attitude'];
 $description = $_POST['description'];
 
-echo $object;
-echo $attitude;
-echo $description;
+// Connect to server and select the chosen database
+$pdo = new PDO("mysql:host=$host;dbname=$database", $username, $password, array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ));
+
+try{
+    $query = $pdo->prepare("insert into dreams (object,attitude,description)
+    values (:object,:attitude,:description)");
+    $data = array(
+    ':object' => $object,
+    ':attitude' => $attitude,
+    ':description' => $description
+    );
+    $query->execute($data);
+    echo "Data successfully inserted into the database table ...";
+    }catch(PDOException $e){
+    echo "Error! failed to insert into the database table :".$e->getMessage();
+}
 
 ?>
